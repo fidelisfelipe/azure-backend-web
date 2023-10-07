774d6ee508d27da340a2acebb320be19ced2d13a
fidelis.guimaraes <atosfiel@gmail.com>
2023-10-07 13:43:32 +0100

    init project

---
+++ src/main/java/dev/fidelisdelipe/web/azurebackendweb/AzureBackendWebApplication.java
@@ -0,0 +1,13 @@
+package dev.fidelisdelipe.web.azurebackendweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureBackendWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureBackendWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/fidelisdelipe/web/azurebackendweb/AzureBackendWebApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fidelisdelipe.web.azurebackendweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureBackendWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
